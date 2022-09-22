5bb4a5c8a47c95a35e7cdfda953b36a9f4c7179b
tayfun <bekzodalimuhamedov@gmail.com>
2022-09-22 16:23:32 +0500

    Фиксация изменений на github [#243564]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Bekzod Alimukhamedov");
+        System.out.println("17.03.1996");
+    }
+}

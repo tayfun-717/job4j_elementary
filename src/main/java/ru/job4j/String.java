b4ddba7716f88b3d52ffba0c8514bc043fe976ce
tayfun <bekzodalimuhamedov@gmail.com>
2022-10-27 15:58:53 +0500

    sting value commit [#243564]

--- src/main/java/ru/job4j/String.java
+++ src/main/java/ru/job4j/String.java
@@ -2,7 +2,9 @@ package ru.job4j;
 
 public class String {
     public static void main (java.lang.String[] args) {
-        java.lang.String test = "tayfun";
-        System.out.println(test);
+        java.lang.String name = "tayfun";
+        int son = 17;
+        java.lang.String car = "Nexia 3";
+        System.out.println(name + "'s favorite number is " + son + " and car is " + car);
     }
 }

74ed2ec64ebc5cde9706c8139a6a5893b12a3c0d
tayfun <bekzodalimuhamedov@gmail.com>
2022-10-27 15:49:22 +0500

    converter value commit [#243564]

---
+++ src/main/java/ru/job4j/String.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class String {
+    public static void main (java.lang.String[] args) {
+        java.lang.String test = "tayfun";
+        System.out.println(test);
+    }
+}
